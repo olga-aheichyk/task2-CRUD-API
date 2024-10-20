3ec1d70bbea0152bbd9803ddcdf22403a4d53bb1
olga.aheichyk <olga.aheichyk@gmail.com>
2024-10-20 21:20:47 +0200

    feat: implement endpoint api/users

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,60 +1,37 @@
 import { IncomingMessage, ServerResponse } from 'node:http';
 import { Message } from '../constants/messages.js';
-
-interface User {
-	id: string;
-	username: string;
-	age: number;
-	hobbies: string[];
-}
-
-let users: User[] = [];
+import { createNewUserOnServer, deleteUserFromServer, getAllUsersFromServer, getUserByIdFromServer, updateUserOnServer } from '../serverOperations/userOperations.js';
 
 export const handleUserRoutes = (request: IncomingMessage, response: ServerResponse): void => {
 	const { method, url } = request;
-	let bodyChunks: Buffer[] = [];
 
-	request.on('data', (chunk: Buffer): void => {
-		bodyChunks.push(chunk);
-	}).on('end', () => {
-		const body: string = Buffer.concat(bodyChunks).toString();
-		response.setHeader('Content-Type', 'application/json');
+	response.setHeader('Content-Type', 'application/json');
 
-		if (url === '/api/users' && method === 'GET') {
-			response.writeHead(200);
-			response.end(JSON.stringify(users));
-		}
+	if (url === '/api/users' && method === 'GET') {
+		getAllUsersFromServer(response);
+	}
 
-		else if (url === '/api/users' && method === 'POST') {
-			const user = JSON.parse(body);
-			users.push(user);
+	else if (url === '/api/users' && method === 'POST') {
+		createNewUserOnServer(request, response);
+	}
 
-			response.writeHead(201);
-			response.end(JSON.stringify({ message: Message.UserAddedSuccessfully, user }));
-		}
+	else if (url?.match(/^\/api\/users\/[a-zA-Z0-9-]+$/) && method === 'GET') {
+		const id = url.split('/')[3];
+		getUserByIdFromServer(id, response);
+	}
 
-		else if (url?.match(/^\/api\/users\/[a-zA-Z0-9-]+$/) && method === 'GET') {
-			const id = url.split('/')[3];
-			users.find((user) => user.id === id);
+	else if (url?.match(/^\/api\/users\/[a-zA-Z0-9-]+$/) && method === 'DELETE') {
+		const id = url.split('/')[3];
+		deleteUserFromServer(id, response);
+	}
 
-			response.writeHead(200);
-			response.end(JSON.stringify(users));
-		}
+	else if (url?.match(/^\/api\/users\/[a-zA-Z0-9-]+$/) && method === 'PUT') {
+		const id = url.split('/')[3];
+		updateUserOnServer(id, request, response);
+	}
 
-		else if (url?.match(/^\/api\/users\/[a-zA-Z0-9-]+$/) && method === 'DELETE') {
-			const id = url.split('/')[3];
-			const index = users.findIndex((user) => user.id === id);
-			users.splice(index, 1);
-
-			response.writeHead(204);
-			response.end(JSON.stringify({ message: Message.UserDeletedSuccesfully}));
-		}
-
-		// else if (url?.match(/^\/api\/users\/[a-zA-Z0-9-]+$/) && method === 'PUT') {}
-
-		else {
-			response.writeHead(404);
-			response.end(JSON.stringify({ message: Message.RouteNotFound }));
-		}
- 	})
+	else {
+		response.writeHead(404);
+		response.end(JSON.stringify({ message: Message.NotFoundError }));
+	}
 }
